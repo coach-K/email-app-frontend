456efd998a811d5ce1f4cbcf2c656e071abeb975
Kehinde Ogunde <codekenn@gmail.com>
2024-05-22 13:29:47 +0100

    Add sender to inbox

--- app/inbox/page.tsx
+++ app/inbox/page.tsx
@@ -1,6 +1,8 @@
+import clsx from "clsx";
 import Link from "next/link";
 import { Card, CardHeader, CardText, CardTitle, ListGroup, ListGroupItem } from "react-bootstrap";
 import { fetchMessages } from "../lib/data";
+import { sender } from "../lib/sender";
 import styles from "./page.module.css";
 
 export default async function Inbox() {
@@ -16,10 +18,17 @@ export default async function Inbox() {
         {messageList && messageList.length > 0 ? messageList.map((message, index) => {
           return <ListGroupItem key={index} variant={message.isRead ? '' : 'primary'}>
           <Link href={`/viewdetail/${message?.id}`}>
-            <CardTitle>{message.subject}</CardTitle>
-            <CardText className={styles.content}>
-            {message.content}
-            </CardText>
+            <div className="d-flex">
+              <div className={clsx("me-3", styles.circleAvatar)}>
+                {sender.initial}
+              </div>
+              <div>
+                <CardTitle>{message.subject}</CardTitle>
+                <CardText className={styles.content}>
+                {message.content}
+                </CardText>
+              </div>
+            </div>
           </Link>
         </ListGroupItem>
         }) 
