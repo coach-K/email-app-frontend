c981c430880c22191e8e49ef524403d8e44d13c0
Kehinde Ogunde <codekenn@gmail.com>
2024-05-23 10:40:12 +0100

    Update inbox page

--- app/inbox/page.tsx
+++ app/inbox/page.tsx
@@ -2,13 +2,13 @@ import clsx from "clsx";
 import Link from "next/link";
 import { Card, CardHeader, CardText, CardTitle, ListGroup, ListGroupItem } from "react-bootstrap";
 import { fetchMessages } from "../lib/data";
-import { sender } from "../lib/sender";
 import styles from "./page.module.css";
 
 export default async function Inbox() {
   const response = await fetchMessages();
   const userMessage = response?.data;
   const messageList = userMessage?.messages;
+  const colors = [styles.red, styles.green, styles.purple];
 
   return (
     <div className="m-2">
@@ -16,11 +16,13 @@ export default async function Inbox() {
       <CardHeader>Inbox</CardHeader>
       <ListGroup variant="flush">
         {messageList && messageList.length > 0 ? messageList.map((message, index) => {
+          const initial =`${message?.sender?.firstname.charAt(0)}${message?.sender?.lastname.charAt(0)}`;
           return <ListGroupItem key={index} variant={message.isRead ? '' : 'primary'}>
           <Link href={`/viewdetail/${message?.id}`}>
             <div className="d-flex">
-              <div className={clsx("me-3", styles.circleAvatar)}>
-                {sender.initial}
+              <div 
+                className={clsx("me-3", styles.circleAvatar, colors[Math.floor(Math.random()*colors.length)])} >
+                {initial}
               </div>
               <div>
                 <CardTitle>{message.subject}</CardTitle>
